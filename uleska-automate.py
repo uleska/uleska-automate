8f26370a9100344bed0e1fef1846448e9a9bdf66
uleska-demo <demo@uleska.com>
2021-06-21 10:43:02 +0100

    Update for multiple scans running

--- uleska-automate.py
+++ uleska-automate.py
@@ -315,21 +315,26 @@ def run_scan_blocking(host, application, version, token):
             scanfinished = True
             break
         
+        versions_running = []
+
         for scan in running_scans_json:
             if 'versionId' in scan:
-                if scan['versionId'] == version:
-                    ### our scan is still running, sleep and return
-                    print ("Our Toolkit " + version + " is still running, waiting...\n")
-                    time.sleep(10)
-                    
-                else:
-                    
-                    ### our scan isn't running
-                     print ("Our Toolkit " + version + " has completed\n")
-                     scanfinished = True
-                     break
+
+                versions_running.append(scan['versionId'])
+
             else:
                 print ("No versionId in the scan\n")
+
+        print("DEBUG: Versions running = " + str(versions_running) )
+
+        if version in versions_running:
+            print ("Our Toolkit " + version + " is still running, waiting...\n")
+            time.sleep(10)
+        else:
+            print ("Our Toolkit " + version + " has completed\n")
+            scanfinished = True
+            break
+
                 
                 
 
